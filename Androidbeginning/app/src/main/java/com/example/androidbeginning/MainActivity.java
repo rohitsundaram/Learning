241015e800c523327f945926ed154ba86d8495f1
ShreekarMS <68714036+ShreekarMS@users.noreply.github.com>
2021-04-01 19:22:40 +0530

    Committed successfully

---
+++ Androidbeginning/app/src/main/java/com/example/androidbeginning/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.androidbeginning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread thread=new Thread()
+        {
+          public void run()
+          {
+              try{
+                  sleep(7000);
+              }
+              catch(Exception e)
+              {
+                  e.printStackTrace();
+              }
+              finally
+              {
+                Intent welcomeIntent=new Intent(MainActivity.this,WelcomeActivity.class);
+                startActivity(welcomeIntent);
+              }
+          }
+        };
+        thread.start();
+    }
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        finish();
+    }
+}
